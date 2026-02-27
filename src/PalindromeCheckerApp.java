ee049060c99c34176bbfae7214b99b000c605344
mano30-4 <manohargajula03@gmail.com>
2026-02-27 16:38:46 +0530

    uc5

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,29 +3,6 @@ import java.util.Stack;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        String word = "level";
-
-        char[] letters = word.toCharArray();
-
-        int start = 0;
-        int end = letters.length - 1;
-        boolean isPalindrome = true;
-
-        while (start < end) {
-            if (letters[start] != letters[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-
-        if (isPalindrome) {
-            System.out.println("Word: " + word);
-            System.out.println("Result: It IS a palindrome.");
-        } else {
-            System.out.println("Word: " + word);
-            System.out.println("Result: It is NOT a palindrome.");
 
 
             String input = "noon";
