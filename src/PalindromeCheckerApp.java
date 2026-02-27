018031f9055ab06a3c0881fa2e5d8d58d1f4bfb3
mano30-4 <manohargajula03@gmail.com>
2026-02-27 16:14:36 +0530

    usecase2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,6 +4,19 @@ public class PalindromeCheckerApp {
         System.out.println("welcome to panlindrome checker management app");
         System.out.println("version 1.0");
         System.out.println("system initailised succesfully");
+        String word = "madam";
+
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println("Word: " + word);
+            System.out.println("Result: It IS a palindrome.");
+        } else {
+            System.out.println("Word: " + word);
+            System.out.println("Result: It is NOT a palindrome.");
 
     }
 }
