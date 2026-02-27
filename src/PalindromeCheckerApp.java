0ed98d6ffef3975d4ef0695e263fb8aed7027603
mano30-4 <manohargajula03@gmail.com>
2026-02-27 15:47:03 +0530

    usecase3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -19,6 +19,21 @@ public class PalindromeCheckerApp {
             System.out.println("Word: " + word);
             System.out.println("Result: It is NOT a palindrome.");
 
+            String word = "racecar";
+
+            String reversed = "";
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reversed += word.charAt(i);
+            }
+
+            if (word.equals(reversed)) {
+                System.out.println("Word: " + word);
+                System.out.println("Result: It IS a palindrome.");
+            } else {
+                System.out.println("Word: " + word);
+                System.out.println("Result: It is NOT a palindrome.");
+            }
+
         }
     }
 }
