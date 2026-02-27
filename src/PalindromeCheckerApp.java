a9b365fa7a3bae70206f328ced0321a2c2d9c8e1
mano30-4 <manohargajula03@gmail.com>
2026-02-27 16:06:46 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,33 +5,6 @@ public class PalindromeCheckerApp {
         System.out.println("version 1.0");
         System.out.println("system initailised succesfully");
 
-        String word = "madam";
 
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
-        }
-
-        if (word.equals(reversed)) {
-            System.out.println("Word: " + word);
-            System.out.println("Result: It IS a palindrome.");
-        } else {
-            System.out.println("Word: " + word);
-            System.out.println("Result: It is NOT a palindrome.");
-
-            String word = "racecar";
-
-            String reversed = "";
-            for (int i = word.length() - 1; i >= 0; i--) {
-                reversed += word.charAt(i);
-            }
-
-            if (word.equals(reversed)) {
-                System.out.println("Word: " + word);
-                System.out.println("Result: It IS a palindrome.");
-            } else {
-                System.out.println("Word: " + word);
-                System.out.println("Result: It is NOT a palindrom");
-            }
     }
 }
