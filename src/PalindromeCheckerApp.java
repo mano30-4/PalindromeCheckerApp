0ccfa3a6ecd99231be7d7fff367aa8f267788db0
mano30-4 <manohargajula03@gmail.com>
2026-02-27 15:35:34 +0530

    use_case_2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,24 @@
 public class PalindromeCheckerApp {
 
-        public static void main(String[] args){
-            System.out.println("welcome to panlindrome checker management app");
-            System.out.println("version 1.0");
-            System.out.println("system initailised succesfully");
-}
+    public static void main(String[] args) {
+        System.out.println("welcome to panlindrome checker management app");
+        System.out.println("version 1.0");
+        System.out.println("system initailised succesfully");
+
+        String word = "madam";
+
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println("Word: " + word);
+            System.out.println("Result: It IS a palindrome.");
+        } else {
+            System.out.println("Word: " + word);
+            System.out.println("Result: It is NOT a palindrome.");
+
+        }
+    }
 }
