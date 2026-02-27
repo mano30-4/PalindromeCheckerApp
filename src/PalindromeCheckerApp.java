3e4a43e8a7920c0e9d68c75593844371745d04a9
mano30-4 <manohargajula03@gmail.com>
2026-02-27 15:16:56 +0530

    Base application setup

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+
+        public static void main(String[] args){
+            System.out.println("welcome to panlindrome checker management app");
+            System.out.println("version 1.0");
+            System.out.println("system initailised succesfully");
+}
 }
